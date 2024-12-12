c9ee121fccd1af7f6f121ae0880e7e6c07b3d743
Muhammad Bin Ali <muhammadbinali706@gmail.com>
2024-12-12 10:23:47 -0500

    Initial commit

---
+++ model.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+
+class ResidualLayer(nn.Module):
+    def __init__(self, dilations, num_channels, kernel_size):
+        super().init()
+        self.conv_tanh = nn.Conv1d(dilation=dilations, in_channels=1, out_channels=num_channels, kernel_size=kernel_size)
+        self.conv_sigm = nn.Conv1d(dilation=dilations, in_channels=1, out_channels=num_channels, kernel_size=kernel_size)
+        self.residual = nn.Conv1d(dilation=dilations, in_channels=num_channels, out_channels=num_channels, kernel_size=1)
+
+    def forward(self, input):
+        out1 = self.conv_tanh(input)
+        out2 = self.conv_sigm(input)
+        out = torch.tanh(out1) * torch.sigmoid(out2)
+        skip = out
+        out = self.residual(out)
+        out = out + input[:, :, -out.size(2) :]
+        return self.residual(out), skip
+
+
+class WaveNetModified(nn.Module):
+    def __init__(self, num_channels, dilation_depth, num_repeat, kernel_size=2):
+        super().__init__()
+        dilations = [2**depth for depth in range(dilation_depth)] * num_repeat
+
+        self.causalConv = nn.Conv1d(in_channels=num_channels, out_channels=num_channels, kernel_size=kernel_size)
+        self.res_layers = nn.ModuleList
+        for dilation_arr in dilations:
+            for depth in dilation_arr:
+                res_layer = ResidualLayer(dilations=depth, num_channels=num_channels, kernel_size=kernel_size)
+                self.res_layers.append(res_layer)
+
+        # num_channels * num_repeat * dilation_depth gives out total number of outputs from our Residual Stack
+        self.linearMix = nn.Conv1d(in_channels=num_channels * num_repeat * dilation_depth, out_channels=1, kernel_size=1)
+
+    def forward(self, input):
+        skips = []
+
+        out = self.causalConv(input)
+
+        for res_layer in self.res_layers:
+            out, skip = res_layer(out)
+            skips.append(skip)
