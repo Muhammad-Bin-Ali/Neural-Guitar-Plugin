fc33f4f177ae610dc2733832cc604f6154446fc1
Muhammad Bin Ali <muhammadbinali706@gmail.com>
2024-12-13 12:05:19 -0500

    Rename as well

--- src/train.py
+++ src/train.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--max_epochs", type=int, default=1_500)
     parser.add_argument("--data", default="data.pickle")
-    
+
     parser.add_argument("--accelerator", default=None)
     parser.add_argument("--devices", default=None)
 
